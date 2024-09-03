0944112884552bb677f7ab06cf4138c355927914
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2024-09-03 01:37:05 -0400

    cleanup identifier; fix examples
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

--- config/external_name.go
+++ config/external_name.go
@@ -10,8 +10,7 @@ import "github.com/crossplane/upjet/pkg/config"
 // provider.
 var ExternalNameConfigs = map[string]config.ExternalName{
 	// Import requires using a randomly generated ID from provider: nl-2e21sda
-	"null_resource": config.IdentifierFromProvider,
-	"ns1_zone":      config.NameAsIdentifier,
+	"ns1_zone": config.IdentifierFromProvider,
 }
 
 // ExternalNameConfigurations applies all external name configs listed in the
